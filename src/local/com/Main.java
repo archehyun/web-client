709b49c350e59422c626214c596ebd14f877a593
LDCC <LDCC@15Z990-G>
2020-08-13 17:21:05 +0900

    first

---
+++ src/local/com/Main.java
@@ -0,0 +1,121 @@
+package local.com;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.json.JSONObject;
+
+public class Main implements Runnable {
+
+	Thread thread;
+	URL url;
+
+	String urlString;
+	boolean flag = false;
+
+	private static String getStringFromInputStream(InputStream is) {
+
+		BufferedReader br = null;
+
+		StringBuilder sb = new StringBuilder();
+
+		String line;
+
+		try {
+
+			br = new BufferedReader(new InputStreamReader(is));
+
+			while ((line = br.readLine()) != null) {
+
+				sb.append(line);
+
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			if (br != null) {
+
+				try {
+
+					br.close();
+
+				} catch (IOException e) {
+
+					e.printStackTrace();
+
+				}
+			}
+		}
+
+		return sb.toString();
+
+	}
+
+	public void callAPI(int temp, int hit) throws IOException {
+
+			System.out.println("call api : " + temp + ", " + hit);
+			url = new URL(urlString + "?name=pch" + "&temp=" + temp + "&hit=" + hit);
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+			JSONObject json = new JSONObject(getStringFromInputStream(in));
+
+			System.out.println(json.toString());
+
+
+	}
+
+	public Main(String url) {
+		urlString = url;
+	}
+
+	public void start() {
+		if (thread == null) {
+			flag = true;
+			thread = new Thread(this);
+			thread.start();
+		}
+	}
+
+
+
+	Random rn;
+	@Override
+	public void run() {
+
+		rn = new Random();
+		while (flag) {
+			try {
+				callAPI(rn.nextInt(20), rn.nextInt(20));
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+
+				flag = false;
+				JOptionPane.showMessageDialog(null, e.getMessage());
+				//e.printStackTrace();
+			}
+		}
+
+	}
+
+	public void stop() {
+		flag = false;
+		thread = null;
+
+	}
+
+}
---
+++ src/local/view/MainFrame.java
@@ -0,0 +1,79 @@
+package local.view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import local.com.Main;
+
+public class MainFrame extends JFrame implements ActionListener {
+
+	Main main;
+
+	String url;
+
+	private JButton butStart;
+
+	private JButton butStop;
+	private JComponent buildMain() {
+		JPanel pnMain = new JPanel(new BorderLayout());
+
+		JPanel pnContorl = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		butStart = new JButton("Start");
+		butStop = new JButton("Stop");
+		butStop.setEnabled(false);
+		butStart.addActionListener(this);
+		butStop.addActionListener(this);
+		pnContorl.add(butStart);
+		pnContorl.add(butStop);
+
+		JPanel pnUrl = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		pnUrl.add(new JLabel("URL:"));
+		pnUrl.add(new JLabel(url));
+
+		pnMain.add(pnUrl);
+		pnMain.add(pnContorl, BorderLayout.SOUTH);
+		return pnMain;
+
+	}
+
+	public MainFrame(String url) {
+
+		this.url = url;
+		main = new Main(url);
+
+		this.getContentPane().add(buildMain());
+		//		this.setSize(new Dimension(200, 100));
+		this.setLocation(500, 200);
+		pack();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		if (command.equals("Start")) {
+			main.start();
+			butStop.setEnabled(true);
+			butStart.setEnabled(false);
+		} else {
+			main.stop();
+			butStop.setEnabled(false);
+			butStart.setEnabled(true);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new MainFrame("http://localhost:8080/web/api");
+	}
+
+}
